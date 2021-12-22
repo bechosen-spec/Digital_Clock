002a0d6cc2e886a935e85441e18d029fbed5b302
bechosen-spec <65294466+bechosen-spec@users.noreply.github.com>
2021-12-23 00:51:59 +0100

    added a welcome message

--- digitalClock.py
+++ digitalClock.py
@@ -1,7 +1,11 @@
 from tkinter import Tk, Label
 
 window = Tk()
-window.title("Digital Clock")
+window.title("Bechosen Digital Clock")
 window.geometry("600x300")
+window.configure(bg="steelblue")
+
+Label = Label(window, text="Welcome!", font=("Arial Black", 78, "bold"), bg="steelblue", fg="White")
+Label.pack(pady=100)
 
 window.mainloop()
